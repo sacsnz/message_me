928a2656cd5160e8868b49d466570843173cc88d
“Isaac” <sales@worksites.co.nz>
2019-10-02 09:50:11 +0000

    Setup most of app including realtime chat

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Message.create(body: "Hi all, I'm in the MessageMe app", user: User.find(2))
+Message.create(body: "Hi, I'm using it too!", user: User.find(3))
+Message.create(body: "What's the MessageMe app all about?", user: User.find(4))
+Message.create(body: "It's just Isaac learning how to code in Ruby on Rails", user: User.find(5))
+Message.create(body: "Cool :)", user: User.find(4))
